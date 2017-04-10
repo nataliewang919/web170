917dff45078506196e4f5c7c2e4417c0506cdbe3
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-04-09 22:38:49 -0700

    this is the first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Han Wang-Web170</title>
+<meta charset="utf-8" />
+<meta name="robots" content="noindex,nofollow" />
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" href="css/portal.css" />
+</head>
+
+<!-- START Header-->
+<body> 
+<header>
+<h1 class="masthead">Web170:Han Wang</h1>
+</header>
+<!-- END Header -->
+
+<!-- START Main Contents -->
+<div class="main">
+<h2>Templates</h2>
+<ul>
+ <li><a href="templates/home.php">View</a></li>   
+ <li><a href="" target="_blank">Code</a></li>       
+</ul>
+
+<h2>Protosite</h2>
+<ul>
+ <li><a href="protosite/">View</a></li>   
+ <li><a href="" target="_blank">Code</a></li>       
+</ul>    
+</div>
+<!-- END Main Contents -->
+
+<!-- Start Footer -->
+<footer>
+<small>
+&copy; 2017, Han Wang All Rights Reserved
+    <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML</a> ~ 
+    <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a> 
+</small>
+</footer>
+<!-- END Footer -->
+
+</body>
+</html>
---
+++ templates/main.php
@@ -0,0 +1,29 @@
+<?php include ("includes/top.php");?>
+<!-- START LEFT Column -->
+<main>
+<article class=mainpage>
+  <h2>Old Peking</h2>
+  <section>
+  <div class="indexpic">
+  <img alt="indexpic" src="images/oldbeijingnew.jpg">
+  </div>
+    <p>Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, `and what is the use of a book,' thought Alice `without pictures or conversation?'</p>
+    <p>So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.</p>
+    <p>There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, `Oh dear! Oh dear! I shall be late!' (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge. </p>
+    <p>In another moment down went Alice after it, never once considering how in the world she was to get out again.</p>
+    <p>The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well.</p>
+  </section>
+</article>
+<!-- END Left Column --> 
+<!-- START Aside our Right Column -->
+<aside>
+  <h2>New Beijing</h2>
+   <div class="indexpic">
+  <img alt="indexpic" src="images/newbeijing.png">
+  </div>
+  <p>So questions one of England's sweetest singers. The `How?' has already been told, after a fashion, in the verses prefixed to Alice in Wonderland ; and some other memories of that happy summer day are set down, for those who care to see them, in this little book--the germ that was to grow into the published volume. But the `Why?' cannot, and need not, be put into words. Those for whom a child's mind is a sealed book, and who see no divinity in a child's smile, would read such words in vain: while for any one that has ever loved one true child, no words are needed. For he will have known the awe that falls on one in the presence of a spirit fresh from GOD's hands, on whom no shadow of sin, and but the outermost fringe of the shadow of sorrow, has yet fallen: he will have felt the bitter contrast between the haunting selfishness that spoils his best deeds and the life that is but an overflowing love--for I think a child's first attitude to the world is a simple love for all living things: and he will have learned that the best work a man can do is when he works for love's sake only, with no thought of name, or gain, or earthly reward. No deed of ours, I suppose, on this side the grave, is really unselfish: yet if one can put forth all one's powers in a task where nothing of reward is hoped for but a little child's whispered thanks, and the airy touch of a little child's pure lips, one seems to come somewhere near to this.</p>
+  <p>There was no idea of publication in my mind when I wrote this little book: that was wholly an afterthought, pressed on me by the `perhaps too partial friends' who always have to bear the blame when a writer rushes into print: and I can truly say that no praise of theirs has ever given me one hundredth part of the pleasure it has been to think of the sick children in hospitals (where it has been a delight to me to send copies) forgetting, for a few bright hours, their pain and weariness--perhaps thinking lovingly of the unknown writer of the tale--perhaps even putting up a childish prayer (and oh, how much it needs!) for one who can but dimly hope to stand, some day, not quite out of sight of those pure young faces, before the great white throne. `I am very sure,' writes a lady-visitor at a Home for Sick Children, `that there will be many loving earnest prayers for you on Easter morning from the children.' . </p>
+</aside>
+</main>
+<!-- END Aside our Right Column-->
+<?php include ("includes/bottom.php");?>
